3631fd10112df6fa6d76fa74dd8ba806a8101b23
Federico Perina <perafede80@gmail.com>
2025-02-19 19:57:25 +0100

    Update the App.tsx file for rendering the survivor creation form

--- frontend/zssn/src/App.tsx
+++ frontend/zssn/src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import SurvivorList from './components/SurvivorList';
+import SurvivorForm from './components/SurvivorForm';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <div>
+          <SurvivorForm />
           <SurvivorList />
         </div>
       </header>
