2fc3f518b23cdc640b9394a7d75941af5fab2e65
Federico Perina <perafede80@gmail.com>
2025-02-19 19:58:15 +0100

    Create separate file to manage all the api requests

--- frontend/zssn/src/api/survivorApi.ts
+++ frontend/zssn/src/api/survivorApi.ts
@@ -3,3 +3,16 @@ export const fetchSurvivors = async () => {
     if (!response.ok) throw new Error('Failed to fetch survivors');
     return response.json();
 };
+
+export const createSurvivor = async (survivor: any) => {
+    const response = await fetch('http://localhost:8000/api/survivors/', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(survivor),
+    });
+
+    if (!response.ok) throw new Error("Failed to create survivor");
+    return response.json();
+};
