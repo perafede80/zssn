44dddc11dc54f2637a134e33fc2df7c5b868868a
Federico Perina <perafede80@gmail.com>
2025-02-20 22:49:53 +0100

    Add new routes

--- frontend/zssn/src/App.tsx
+++ frontend/zssn/src/App.tsx
@@ -1,18 +1,22 @@
-import React from 'react';
-import './App.css';
-import SurvivorList from './components/SurvivorList';
-import SurvivorForm from './components/SurvivorForm';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+import SurvivorForm from "./components/SurvivorForm";
+import SurvivorList from "./components/SurvivorList";
+import SurvivorDetail from "./pages/SurvivorDetails";
+import UpdateLocation from "./pages/UpdateLocation";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <div>
-          <SurvivorForm />
-          <SurvivorList />
-        </div>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/create-survivor" element={<SurvivorForm />} />
+        <Route path="/survivors" element={<SurvivorList />} />
+        <Route path="/survivors/:id" element={<SurvivorDetail />} />
+        <Route path="/update-location/:id" element={<UpdateLocation />} />
+      </Routes>
+    </Router>
   );
 }
 
