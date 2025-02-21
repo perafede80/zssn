988ec3c9a54c95309144da7d199bb0e2f6d4ab82
Federico Perina <perafede80@gmail.com>
2025-02-21 09:52:27 +0100

    Update: wrong App import statement

--- frontend/zssn/src/App.test.tsx
+++ frontend/zssn/src/App.test.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
-import App from "../App";
+import App from "./App";
 
 describe("App Routing", () => {
     it("renders Landing Page on default route `/`", () => {
