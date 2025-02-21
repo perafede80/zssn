a59780fa797a053deb7029bbfdf31415c46518dd
Federico Perina <perafede80@gmail.com>
2025-02-21 09:48:24 +0100

    Update: Create trade api endpoint

--- frontend/zssn/src/api/survivorApi.ts
+++ frontend/zssn/src/api/survivorApi.ts
@@ -42,3 +42,33 @@ export const updateSurvivorLocation = async (id: string, latitude: number, longi
 
     return response.json();
 };
+
+export interface TradeItems {
+    [key: string]: number; // Item name as key, quantity as value
+}
+
+export const survivorTradeItems = async (
+    survivorAId: number,
+    survivorBId: number,
+    itemsA: TradeItems,
+    itemsB: TradeItems
+) => {
+    const tradePayload = {
+        survivor_b_id: survivorBId,
+        items_a: itemsA,
+        items_b: itemsB,
+    };
+    const response = await fetch(`http://localhost:8000/api/survivors/${survivorAId}/trade/`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(tradePayload),
+    });
+
+    if (!response.ok) {
+        throw new Error("Trade failed");
+    }
+
+    return response.json();
+};
