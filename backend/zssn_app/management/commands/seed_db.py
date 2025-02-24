70c2c9c3e329f16742b33b695cc8c9bce21ffde5
Federico Perina <perafede80@gmail.com>
2025-02-24 07:37:12 +0100

    Update: fix the seeder to avoid creating duplicates

--- backend/zssn_app/management/commands/seed_db.py
+++ backend/zssn_app/management/commands/seed_db.py
@@ -5,6 +5,7 @@ import logging
 
 logger = logging.getLogger(__name__)
 
+
 class Command(BaseCommand):
     help = 'Seed the database with at least 10 survivor records'
 
@@ -23,8 +24,8 @@ class Command(BaseCommand):
             )
 
             # Add random inventory items for each survivor
-            for _ in range(random.randint(1, 5)):
-                Inventory.objects.create(
+            for _ in range(random.randint(1, 4)):
+                self.add_or_update_inventory(
                     survivor=survivor,
                     item=random.choice([Item.WATER, Item.FOOD, Item.MEDICATION, Item.AMMUNITION]),
                     quantity=random.randint(1, 10)
@@ -32,3 +33,18 @@ class Command(BaseCommand):
 
         logger.debug('Successfully added 10 survivors with inventory!')
 
+    def add_or_update_inventory(self, survivor, item, quantity):
+        """
+        Adds a new inventory item for a survivor or updates the quantity if the item already exists.
+        """
+        # Check if an inventory item of this type already exists for the survivor
+        inventory_item, created = Inventory.objects.get_or_create(
+            survivor=survivor,
+            item=item,
+            defaults={'quantity': quantity}
+        )
+
+        # If the inventory item already exists, update its quantity
+        if not created:
+            inventory_item.quantity += quantity
+            inventory_item.save()
